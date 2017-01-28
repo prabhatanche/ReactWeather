563faf09202a60bd4b263ad69b8dafe667c7ed09
prabhat anche <prabhatanche@gmail.com>
2017-01-27 23:26:12 -0500

    CSS-Foundation examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,12 +11,27 @@
 // module.exports = Examples;
 
 var React = require('react');
-
+var {Link} = require('react-router');
 var Examples = (props)=>{
     return(
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to examples page</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are few locations to try out!!!</p>
+        <ol>
+          <li>
+            <Link to='/?location=New Jersey'>New Jersey, NJ</Link>
+          </li>
+          <li>
+            <Link to='/?location= Hyderabad'>Hyderabad, HYD(IND)</Link>
+          </li>
+
+        </ol>
+
+
+
+
+
+
       </div>
     );
   }
